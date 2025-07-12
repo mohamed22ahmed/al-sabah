0a2d96ed066ff73bcdb19c1da0a921065d939b03
mohamed22ahmed <mohammedahmedhammam113@gmail.com>
2025-07-12 04:05:50 +0300

    redirect to dashboard and auto login when asking for login route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,18 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\LoginRequest;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function loginUser()
     {
+        if(Auth::check()){
+            return redirect()->route('categories.index');
+        }
         return Inertia::render('login', [
             'logo' => asset('images/elsabah.png'),
         ]);
     }
+
     public function login(LoginRequest $request)
     {
         $request->authenticate();
+        return redirect()->route('categories.index');
+    }
+
+    public function dashboard(){
         return redirect()->route('categories.index');
     }
 }
