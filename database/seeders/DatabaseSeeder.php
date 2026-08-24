a42585c94328d197e3b73bf6e66f215ebc8fc68b
mohamed22ahmed <mohammedahmedhammam113@gmail.com>
2026-08-24 13:06:30 +0300

    update composer.json

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             CategoriesSeeder::class,
             ZonesSeeder::class,
-            DistrictsSeeder::class,
         ]);
     }
 }
